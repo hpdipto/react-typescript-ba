f47132a4f63fec5cf94948cd285984db4cd80069
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-08 12:33:05 +0600

    Functional component is used

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { TextField } from "./TextField";
 
 const App: React.FC = () => {
-  return <div>yo</div>;
+  return (
+    <div>
+      <TextField text="hello" person={{ firstName: "", lastName: "" }} />
+    </div>
+  );
 };
 
 export default App;
