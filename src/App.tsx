832f0b194a2c75fdefca502e3095276f3b6891f3
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-08 12:36:56 +0600

    Hooks added with type

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField } from "./TextField";
 
 const App: React.FC = () => {
+  const [count, setCount] = useState<null | number>(5);
+
+  setCount(null);
+  setCount(5);
+
   return (
     <div>
       <TextField text="hello" person={{ firstName: "", lastName: "" }} />
